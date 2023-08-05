956907d6fce2e05df6a4c3b14081639f1d81b8f2
ocing86 <ocing86@gmail.com>
2023-08-06 00:39:16 +0800

    build documentation

---
+++ src/prelude.rs
@@ -0,0 +1,12 @@
+pub use super::core::cb:: {
+    BFRDYN,
+    NecResult,
+    IidxResult
+};
+
+pub use super::core::errors:: {
+    InvalidIndex,
+    NotEnoughCapacity
+};
+
+pub use super::core::helper;
